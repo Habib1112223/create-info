1a5d6cb0c5b8f7cba2965dc56ca0e4092a4b48db
Habibur1112223 <webdevelopment.habib2023@gmail.com>
2023-09-16 19:13:42 +0600

    start the code for heading session

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,15 @@
+
+
+const Header = () => {
+      return (
+            <div>
+                  <div className="pt-[50px] pb-[32px]">
+                        <h1 className="text-3xl text-center font-bold">Course Registation</h1>
+                  </div>
+                  
+            </div>
+
+      );
+};
+
+export default Header;
